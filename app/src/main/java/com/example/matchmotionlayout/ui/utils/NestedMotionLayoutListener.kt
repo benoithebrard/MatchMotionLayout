5e85780db5e36d383a858de8fc16a3ec9a1d23ac
Benoit Hebrard <benoit.hebrard@gmail.com>
2023-09-28 09:14:27 +0200

    extract functionality

---
+++ app/src/main/java/com/example/matchmotionlayout/ui/utils/NestedMotionLayoutListener.kt
@@ -0,0 +1,54 @@
+package com.example.matchmotionlayout.ui.utils
+
+import androidx.constraintlayout.motion.widget.MotionLayout
+import androidx.core.view.children
+import com.example.matchmotionlayout.databinding.FragmentFootballBinding
+
+class NestedMotionLayoutListener(
+    val viewBinding: FragmentFootballBinding
+) : MotionLayout.TransitionListener {
+
+    override fun onTransitionStarted(motionLayout: MotionLayout?, startId: Int, endId: Int) {
+        updateNestedMotionLayout(motionLayout)
+    }
+
+    override fun onTransitionChange(
+        motionLayout: MotionLayout?,
+        startId: Int,
+        endId: Int,
+        progress: Float
+    ) {
+        updateNestedMotionLayout(motionLayout)
+    }
+
+    override fun onTransitionCompleted(motionLayout: MotionLayout?, currentId: Int) {
+        updateNestedMotionLayout(motionLayout)
+    }
+
+    override fun onTransitionTrigger(
+        motionLayout: MotionLayout?,
+        triggerId: Int,
+        positive: Boolean,
+        progress: Float
+    ) {
+        updateNestedMotionLayout(motionLayout)
+    }
+
+    fun setup() {
+        viewBinding.apply {
+            rootContainer.setTransitionListener(this@NestedMotionLayoutListener)
+            (overallHeaderContainer.children.first() as MotionLayout).setTransitionListener(this@NestedMotionLayoutListener)
+        }
+    }
+
+    private fun updateNestedMotionLayout(motionLayout: MotionLayout?) {
+        motionLayout?.let { layout ->
+            viewBinding.apply {
+                if (layout.id == rootContainer.id) {
+                    (overallHeaderContainer.children.first() as MotionLayout).progress =
+                        layout.progress
+                }
+            }
+        }
+    }
+}
